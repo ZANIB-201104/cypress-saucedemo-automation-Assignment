ccece1407bd0be7fa46179ed3473d147343a43bf
ZANIB-201104 <umarduggal394@gmail.com>
2026-09-08 23:04:44 +0500

    Add custom Cypress commands for login and page visit

---
+++ cypress/support/commands.js
@@ -0,0 +1,9 @@
+Cypress.Commands.add('login', (username, password) => {
+    cy.get('[data-test="username"]').clear().type(username);
+    cy.get('[data-test="password"]').clear().type(password);
+    cy.get('[data-test="login-button"]').click();
+});
+
+Cypress.Commands.add('visitLoginPage', () => {
+    cy.visit('/');
+});
